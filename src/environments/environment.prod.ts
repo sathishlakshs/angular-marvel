a6e7b51a040236812958b5f4e3d5f8d5dc14424d
sathishlakshs <sathishkumar3110@gmail.com>
2021-04-12 15:42:28 +0530

    http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api: 'http://gateway.marvel.com/v1/public',
+  api: 'https://gateway.marvel.com/v1/public',
   publickey: '4963464e44c59973e08813ca997524ea',
   privatekey: '6cef8a09bcaa894c1968f3dac39ebbf8a750637b',
   production: true
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://gateway.marvel.com/v1/public',
+  api: 'https://gateway.marvel.com/v1/public',
   publickey: '4963464e44c59973e08813ca997524ea',
   privatekey: '6cef8a09bcaa894c1968f3dac39ebbf8a750637b'
 };
