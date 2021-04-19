950a06b28213e392a20700d1fe957109aa5a0ab8
sathishlakshs <sathishkumar3110@gmail.com>
2021-04-19 15:49:15 +0530

    dev marvel deleted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   api: 'https://gateway.marvel.com/v1/public',
-  publickey: '4963464e44c59973e08813ca997524ea',
-  privatekey: '6cef8a09bcaa894c1968f3dac39ebbf8a750637b',
+  publickey: 'xxxx', // provide your marvel publickey
+  privatekey: 'xxxx', // provide your marvel privatekey
   production: true
 };
